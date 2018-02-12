5dff3a7137496f26bb0dd154592382817b4dc91b
Jacob Huggins <jacob@jacobs-macbook-pro.local>
2018-02-12 16:21:30 +0800

    Initial Commit

---
+++ Password Generator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Password Generator
+//
+//  Created by Jacob Huggins on 12/2/18.
+//  Copyright © 2018 Jacob Huggins. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Password Generator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Password Generator
+//
+//  Created by Jacob Huggins on 12/2/18.
+//  Copyright © 2018 Jacob Huggins. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
